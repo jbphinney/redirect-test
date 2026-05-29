b71ac9d25d5d42355af2411174b7d6f9dcddb891
jbphinney <jbphinney@gmail.com>
2026-05-29 15:45:02 -0500

    another update for the first build

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,13 +3,3 @@
  *
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
-
-exports.onCreateWebpackConfig = ({ actions, stage }) => {
-  if (stage === 'build-javascript' || stage === 'build-html') {
-    actions.setWebpackConfig({
-      optimization: {
-        minimize: stage === 'build-javascript',
-      },
-    })
-  }
-}
