a14909209c377a258e473b4a987b3d2d25293655
jbphinney <jbphinney@gmail.com>
2026-05-29 16:06:02 -0500

    adding a redirect to test

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,3 +3,15 @@
  *
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
+
+exports.createPages = async ({ actions }) => {
+  const { createRedirect } = actions
+
+  createRedirect({
+    fromPath: '/redirect-test-one/',
+    toPath: '/redirect-test-two/',
+    isPermanent: true,
+    redirectInBrowser: true,
+    statusCode: 301,
+  })
+}
